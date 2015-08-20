9a3d5fbcec815cf652796caa6117719ca7177954
bbraithwaite <bradley.braithwaite@gmail.com>
2015-08-20 15:28:58 +0200

    build

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.use(express.static('public'));
 var server = app.listen((process.env.PORT || 3000), function () {
   var host = server.address().address;
   var port = server.address().port;
-
+  
   console.log('Example app listening at http://%s:%s', host, port);
 });
