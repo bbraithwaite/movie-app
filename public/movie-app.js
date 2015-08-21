8ff0ddd1fda02d96020466e2973f4dfbb44edda6
bbraithwaite <bradley.braithwaite@gmail.com>
2015-08-21 14:38:55 +0200

    img fix

--- public/movie-app.js
+++ public/movie-app.js
@@ -17,4 +17,12 @@ angular
   })
   .run(function ($httpBackend) {
     e2eMocks($httpBackend);
-  });
+  })
+  .config(function($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+      // Allow same origin resource loads.
+      'self',
+      // Allow loading from our assets domain.  Notice the difference between * and **.
+      'http://ia.media-imdb.com/**'
+  ]);
+});
