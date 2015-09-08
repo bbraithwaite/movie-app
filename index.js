09f1f7dae84c63a37438d29d7a89b4fd4c0a78ae
Bradley Braithwaite <hi@bradoncode.com>
2015-09-08 15:02:16 -0400

    remove comments

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
-// test
-// tester
+
 app.use(express.static('public'));
 
 var server = app.listen((process.env.PORT || 3000), function () {
